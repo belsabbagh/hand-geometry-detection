3130c7b994167d3f2a6c8002a71e6fe4c2f1b82b
Belal-Elsabbagh <70054230+Belal-Elsabbagh@users.noreply.github.com>
2022-12-25 23:54:44 +0200

    big improvement

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 """Main Module"""
-import copy
-
 import cv2
 import numpy as np
 
@@ -10,31 +8,40 @@ from src.image_ops.scale_img import scale_img
 
 
 def test_3():
-    img_path = r'data\hand.jpg'
-    frame = scale_img(read_img(img_path), 0.4)
-    frame = cv2.bilateralFilter(frame, 5, 50, 100)
-    bgModel = cv2.createBackgroundSubtractorMOG2(0, 50)
-    fgmask = bgModel.apply(frame)
-    kernel = np.ones((3, 3), np.uint8)
-    fgmask = cv2.erode(fgmask, kernel, iterations=1)
-    img = cv2.bitwise_and(frame, frame, mask=fgmask)
-
-    # Skin detect and thresholding
-    skin_mask = mask_color(img, [0, 48, 80], [20, 255, 255])
-    cv2.imshow('Threshold Hands', skin_mask)
-    skin_mask_copy = copy.deepcopy(skin_mask)
-    contours, hierarchy = cv2.findContours(skin_mask_copy, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    img_path = r'data\hand.png'
+    img = scale_img(read_img(img_path), 0.4)
+    processed_img = preprocess(img)
+    contours, _ = cv2.findContours(processed_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    res, hull = draw_hand_shape(contours)
+    status, points = calculate_fingers(res)
     drawing = np.zeros(img.shape, np.uint8)
+    draw_contours(drawing, [res], (0, 255, 0), 2)
+    draw_contours(drawing, [hull], (0, 0, 255), 3)
+    for pt in points:
+        cv2.circle(drawing, pt, 8, [211, 84, 0], -1)
+    print("Fingers", len(points) + 1)
+    cv2.imshow('output', drawing)
+    cv2.waitKey(0)
+
+
+def draw_hand_shape(contours):
     res = None
     if len(contours) > 0:
         res = get_res(contours)
         hull = cv2.convexHull(res)
-        draw_contours(drawing, [res], (0, 255, 0), 2)
-        draw_contours(drawing, [hull], (0, 0, 255), 3)
-    status, count = calculate_fingers(res, drawing)
-    print("Fingers", count)
-    cv2.imshow('output', drawing)
-    cv2.waitKey(0)
+    return res, hull
+
+
+def preprocess(frame):
+    frame = cv2.bilateralFilter(frame, 5, 50, 100)
+    bgModel = cv2.createBackgroundSubtractorMOG2(0, 50)
+    fgmask = bgModel.apply(frame)
+    fgmask = cv2.erode(fgmask, np.ones((3, 3), np.uint8), iterations=1)
+    img = cv2.bitwise_and(frame, frame, mask=fgmask)
+    cv2.imshow('Morph', img)
+    skin_mask = mask_color(img, [0, 48, 80], [20, 255, 255])
+    cv2.imshow('Threshold Hands', skin_mask)
+    return skin_mask
 
 
 def get_res(contours):
@@ -49,8 +56,7 @@ def get_res(contours):
 
 
 def mask_color(img, lower, upper):
-    return cv2.inRange(cv2.cvtColor(img, cv2.COLOR_BGR2HSV), np.array(lower, dtype="uint8"),
-                       np.array(upper, dtype="uint8"))
+    return cv2.inRange(cv2.cvtColor(img, cv2.COLOR_BGR2HSV), np.array(lower, dtype=np.uint8), np.array(upper, dtype=np.uint8))
 
 
 if __name__ == '__main__':
--- src/finger_detector/__init__.py
+++ src/finger_detector/__init__.py
@@ -3,7 +3,7 @@ import math
 import cv2
 
 
-def calculate_fingers(res, drawing):
+def calculate_fingers(res):
     #  convexity defect
     hull = cv2.convexHull(res, returnPoints=False)
     if len(hull) <= 3:
@@ -11,22 +11,26 @@ def calculate_fingers(res, drawing):
     defects = cv2.convexityDefects(res, hull)
     if defects is None:
         return False, 0
-    count = 0
+    points = []
     for i in range(defects.shape[0]):  # calculate the angle
-        s, e, f, d = defects[i][0]
-        angle, far = calculate_angle(e, f, res, s)
+        angle, far = calculate_angle_from_defect(defects[i][0], res)
         if angle <= math.pi / 2:  # angle less than 90 degree, treat as fingers
-            count += 1
-            cv2.circle(drawing, far, 8, [211, 84, 0], -1)
-    return (True, count + 1) if count > 0 else (True, 0)
+            points.append(far)
+    return (True, points) if len(points) > 0 else (True, 0)
 
 
-def calculate_angle(e, f, res, s):
+def calculate_angle(s, e, f, res):
     start = tuple(res[s][0])
     end = tuple(res[e][0])
     far = tuple(res[f][0])
-    a = math.sqrt((end[0] - start[0]) ** 2 + (end[1] - start[1]) ** 2)
-    b = math.sqrt((far[0] - start[0]) ** 2 + (far[1] - start[1]) ** 2)
-    c = math.sqrt((end[0] - far[0]) ** 2 + (end[1] - far[1]) ** 2)
+    a = math.dist(start, end)
+    b = math.dist(start, far)
+    c = math.dist(end, far)
     angle = math.acos((b ** 2 + c ** 2 - a ** 2) / (2 * b * c))  # cosine theorem
+    return angle, far
+
+
+def calculate_angle_from_defect(defect, res):
+    s, e, f, _ = defect
+    angle, far = calculate_angle(s, e, f, res)
     return angle, far
