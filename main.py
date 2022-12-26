aeaf9bb012e717fd9098b2a1a43e27dea0956dfb
Belal-Elsabbagh <70054230+Belal-Elsabbagh@users.noreply.github.com>
2022-12-26 07:54:58 +0200

    wrote directory test

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 """Main Module"""
 
-from test import test_detection_on_image
+from test import test_detection_on_image, test_detection_on_directory
+
 
 if __name__ == '__main__':
-    test_detection_on_image()
+    test_detection_on_directory()
--- test/__init__.py
+++ test/__init__.py
@@ -1,9 +1,20 @@
+import glob
+import os
+
+import cv2
+
 from src.hand_detector import detect_hand_in_image
 from src.image_ops import read_img
 from src.image_ops.scale_img import scale_img
 
 
-def test_detection_on_image():
-    img_path = r'data/palmar left/extra/p/Hand_0000748.jpg'
+
+def test_detection_on_image(img_path):
     img = scale_img(read_img(img_path), 0.4)
     detect_hand_in_image(img)
+
+
+def test_detection_on_directory():
+    dir_path = r'data\dorsal left\p'
+    for f in os.listdir(dir_path):
+        test_detection_on_image(os.path.join(dir_path, f))
